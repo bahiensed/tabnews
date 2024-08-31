9ab875f8d9629db1cb3f4ce9386db84f6ee01afa
dbahiense <douglas@rohling.com.br>
2024-08-31 15:47:08 -0300

    refactor: update database connection to use environment variables

--- db/database.js
+++ db/database.js
@@ -1,9 +1,19 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client();
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  });
   await client.connect();
-  const res = await client.query(queryObject);
+  const result = await client.query(queryObject);
   await client.end();
   return result;
 }
+
+export default {
+  query: query,
+};
