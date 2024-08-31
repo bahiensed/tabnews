9dad22f0b1f6e4cf330c45b87b3b63c5b4d491dd
dbahiense <douglas@rohling.com.br>
2024-08-31 15:48:41 -0300

    test: update API endpoint in integration test to use localhost

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,6 @@
 test("GET /api/v1/status returns code 200", async () => {
   const res = await fetch(
-    "https://cuddly-train-5pgrr6qrgr37vx4-3000.app.github.dev/api/v1/status",
+    "http://localhost:3000/api/v1/status"
   );
   expect(res.status).toBe(200);
 });
