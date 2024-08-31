ef06a875b4b3594b7cbcb2f3a7bdc20d93312f1a
Douglas Bahiense <douglas@rohling.com.br>
2024-08-31 01:11:15 +0000

    test: tests on test file

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,6 @@
 test("GET /api/v1/status returns code 200", async () => {
-  const res = await fetch("https://sotabrasil.com.br/api/v1/status");
+  const res = await fetch(
+    "https://cuddly-train-5pgrr6qrgr37vx4-3000.app.github.dev/api/v1/status",
+  );
   expect(res.status).toBe(200);
 });
